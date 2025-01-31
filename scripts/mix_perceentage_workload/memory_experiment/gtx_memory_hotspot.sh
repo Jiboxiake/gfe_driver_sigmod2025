1eb77b84efa79a425cee71785ce24308f9183473
Jiboxiake <jiboxiake@gmail.com>
2025-01-31 21:54:51 +0000

    update the scripts to measure memory consumption

--- scripts/mix_perceentage_workload/memory_experiment/gtx_memory_hotspot.sh
+++ scripts/mix_perceentage_workload/memory_experiment/gtx_memory_hotspot.sh
@@ -2,27 +2,27 @@ LD_LIBRARY_PATH=/usr/local/lib
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/zhou822/GTX-SIGMOD2025/build/
 export LD_LIBRARY_PATH 
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,bfs,wcc,cdlp,pagerank --mixed_workload true
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,bfs,wcc,cdlp,pagerank --mixed_workload true --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,bfs,wcc,lcc,pagerank --mixed_workload true
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,bfs,wcc,lcc,pagerank --mixed_workload true --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,lcc,wcc,cdlp,pagerank --mixed_workload true
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,lcc,wcc,cdlp,pagerank --mixed_workload true --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,bfs,wcc,cdlp,lcc --mixed_workload true
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist sssp,bfs,wcc,cdlp,lcc --mixed_workload true --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist lcc,bfs,wcc,cdlp,pagerank --mixed_workload true
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/hotspot2.graphlog --aging_timeout 24h -l gtx_rw -w 30 -r 30 --blacklist lcc,bfs,wcc,cdlp,pagerank --mixed_workload true --track_memory true
 echo
 echo '***********************************************************'
 echo
--- scripts/mix_perceentage_workload/memory_experiment/sortledton_memory_normal_repeat.sh
+++ scripts/mix_perceentage_workload/memory_experiment/sortledton_memory_normal_repeat.sh
@@ -1,46 +1,46 @@
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
 echo
 echo '***********************************************************'
 echo
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
-echo
-echo '***********************************************************'
-echo
-sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
-echo
-echo '***********************************************************'
-echo
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
 echo
 echo '***********************************************************'
 echo
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
-echo
-echo '***********************************************************'
-echo
-sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
-echo
-echo '***********************************************************'
-echo
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
 echo
 echo '***********************************************************'
 echo
 sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
-numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
+echo
+echo '***********************************************************'
+echo
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
+echo
+echo '***********************************************************'
+echo
+sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
+echo
+echo '***********************************************************'
+echo
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
+echo
+echo '***********************************************************'
+echo
+sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'
+numactl -N 1 -l /home/zhou822/gfe_driver_sigmod2025/build/gfe_driver -G /ssd_root/zhou822/graph_source/graph500/graph500-24.properties  -R 1 -u --log /home/zhou822/graph_source/test0.graphlog --aging_timeout 24h -l sortledton.4 -w 30 -r 30 --blacklist pagerank,bfs,wcc,cdlp,lcc --mixed_workload true --block_size 512 --track_memory true
 echo
 echo '***********************************************************'
 echo
