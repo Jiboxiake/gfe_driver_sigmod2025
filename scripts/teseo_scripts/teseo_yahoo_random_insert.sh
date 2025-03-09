153f3c354df2aaff25b05b70ac8eafb18479e15e
Jiboxiake <jiboxiake@gmail.com>
2025-03-09 21:34:41 +0000

    minor script fix

--- scripts/teseo_scripts/teseo_yahoo_random_insert.sh
+++ scripts/teseo_scripts/teseo_yahoo_random_insert.sh
@@ -1,3 +1,9 @@
+numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 64 
+numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 56 
+numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 48 
+numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 40 
+numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 32 
+numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 24 
 numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 16 
 numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 12 
 numactl -N 0 -l /home/zhou822/gfe_driver_bigdata/build/gfe_driver -G /home/zhou822/gfe_experiment_data/yahoo-song/out.yahoo-song.el -u -l teseo-lcc.13 -w 8 
